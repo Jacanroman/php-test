b2e60c87b8e31bee3f440e9353e9d53378226ded
Javier <javier@milexagroup.com>
2024-06-02 20:32:14 +0200

    adding the logic to the the required data for an specific pokemon

--- app/Services/UserServices.php
+++ app/Services/UserServices.php
@@ -53,7 +53,37 @@ class UserServices
             return response()->json(['error' => $e->getMessage()], 500);
         }
 
-        dd($id);
+        //do the logic to take the data for the response
+
+        $data =[];
+        if(!is_null($response)){
+            $data['pokemon_name'] = $response['name'];
+            $data['height'] = $response['height'];
+            $data['weight'] = $response['weight'];
+
+            //filter for the abilites using array_filter
+            $filtered = array_filter(array_column($response['abilities'], 'ability'));
+            $abilities = array_filter(array_column($filtered, 'name'));
+
+            $data['abilities'] = $abilities;
+
+            //take the images for the sprites
+
+            foreach ($response['sprites'] as $item){
+                if(!is_null($item) && !is_array($item)){
+                    $data['images'][]=$item;
+                }
+            }
+
+        }
+
+        $final_array=[];
+        $final_array['data'] = $data;
+        $final_array['error'] = (is_null($response)) ? "No data found for that Pokemon" : null;
+
+        dd($final_array);
+
+        //I won't add more data due I don't have enough time
 
         return $final_array;
 
